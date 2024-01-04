d00fba5d9cc3cb83539588bdb47edb76a9479dee
ferry9 <ferry9@gmail.com>
2024-01-04 22:43:49 +0100

    Ajout de la jsDoc

--- controllers/ControllerUsers.js
+++ controllers/ControllerUsers.js
@@ -1,12 +1,26 @@
-// controllers/ControllerUsers.js
+/**
+ * Fonction de connexion.
+ * @param {express.Request} req - L'objet de la requête Express.
+ * @param {express.Response} res - L'objet de la réponse Express.
+ */
 exports.signin = (req, res) => {
     res.send("connexion");
 };
 
+/**
+ * Fonction d'inscription.
+ * @param {express.Request} req - L'objet de la requête Express.
+ * @param {express.Response} res - L'objet de la réponse Express.
+ */
 exports.signup = (req, res) => {
     res.send("inscription");
 };
 
+/**
+ * Fonction du tableau de bord.
+ * @param {express.Request} req - L'objet de la requête Express.
+ * @param {express.Response} res - L'objet de la réponse Express.
+ */
 exports.dashboard = (req, res) => {
     res.send("dashboard");
 };

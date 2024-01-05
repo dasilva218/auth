a11027a61beed89ff3bc59026c500eed7b0b8f42
ferry9 <ferry9@gmail.com>
2024-01-05 01:09:10 +0100

    ajout model et mongo db en local

--- server.js
+++ server.js
@@ -2,11 +2,18 @@
 const express = require('express');
 const app = express();
 
+// connection à ma base de donnée mongodb
+const db = require('./Bd/bd')
+// Appel de la base de donnée
+db();
+
 // Importation des routes
 const routes = require('./routes/routes');
 
 // Utilisation des routes dans l'application
 app.use('/api', routes); // Remplacez "/api" par le chemin que vous souhaitez utiliser pour vos routes
+
+
 
 // Démarrage du serveur au port 3000
 const port = 3000;
